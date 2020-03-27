c406c4d144205e1f4ab9e77f1903f650b9101d4a
Charles <charlesmbrady@gmail.com>
2020-03-27 15:18:19 -0400

    Added Sprint model

---
+++ backend/models/sprint.js
@@ -0,0 +1,29 @@
+const bcrypt = require('bcrypt');
+// const { sequelize, DataTypes } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  const Sprint = sequelize.define('Sprint', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    subject: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      trim: true
+    },
+    description: {
+      type: DataTypes.STRING,
+      trim: true
+    },
+    points: {
+      type: DataTypes.INTEGER
+    },
+    status: {
+      type: DataTypes.STRING
+    }
+  });
+
+  return Sprint;
+};
